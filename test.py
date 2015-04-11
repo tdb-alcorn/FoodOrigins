18fe617de21958a06e6ea5e9cd1156d4dc4253b4
talcorn92 <talcorn.92@gmail.com>
2015-04-11 14:06:40 -0400

    added get_ingredients

--- test.py
+++ test.py
@@ -3,10 +3,17 @@ import requests
 BASE_URL = "http://api.foodessentials.com/"
 DEFAULT_PARAMS = {'appid': 'h3hw38z6ucxzy67rbjpyk8jy',
                   'f': 'json'}
+SESSION_ID = _tomsession()
+
+
+def _tomsession():
+    return createsession('tom', 'test')
+
 
 def get(method, params):
-    r = requests.get(BASE_URL+method, params=params)
+    r = requests.get(BASE_URL+method, params=params, data={'api_key': params['appid']})
     return r
+
 
 def label(sid, barcode):
     params = DEFAULT_PARAMS.copy()
@@ -14,9 +21,14 @@ def label(sid, barcode):
     params['u'] = barcode
     return get('label', params)
 
-def createsession(user_id, device_id, version):
+
+def createsession(user_id, device_id):
     params = DEFAULT_PARAMS.copy()
     params['uid'] = user_id
     params['devid'] = device_id
-    params['v'] = version
-    return get('createsession', params)
+    return get('createsession', params).json()['session_id']
+
+
+def get_ingredients(barcode):
+    r = label(SESSION_ID, barcode)
+    return r.json()
