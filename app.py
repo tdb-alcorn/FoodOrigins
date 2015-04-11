ff3d61cd89862bb8bb0825695584e1da050f1be2
talcorn92 <talcorn.92@gmail.com>
2015-04-11 19:24:20 -0400

    added num_countries parameter to main in app.yp

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from barcodes import get_country
 from origins import origins_us_list
 
 
-def main(barcode):
+def main(barcode, num_countries=10):
     country = get_country(barcode)
     r = get_ingredients(barcode)
     if country == 'USA':
-        origins = origins_us_list(r['ingredients']).to_dict()
+        origins = origins_us_list(r['ingredients'])
+        origins.sort(ascending=False)
+        origins = origins.iloc[:num_countries].to_dict()
     else:
         origins = {}
     return origins
