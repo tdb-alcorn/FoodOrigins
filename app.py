018f71162e1a236cc48141c8bd6dd43d6def77d4
talcorn92 <talcorn.92@gmail.com>
2015-04-11 20:32:50 -0400

    made lightweight webserver using web.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from ingredients import get_ingredients
 from barcodes import get_country
 from origins import origins_us_list
+import web
 
+
+urls = (
+  '/', 'BarcodeServer'
+)
+
+render = web.template.render('static/')
 
 def main(barcode, num_countries=10):
     country = get_country(barcode)
@@ -13,3 +20,18 @@ def main(barcode, num_countries=10):
     else:
         origins = {}
     return origins
+
+
+app = web.application(urls, globals())
+
+class BarcodeServer:
+    def GET(self):
+        return render.form()
+
+    def POST(self):
+        form = web.input(barcode="")
+        origins = main(form.barcode)
+        return render.index(origins=origins)
+
+if __name__ == "__main__":
+    app.run()
